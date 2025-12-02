aca47b2640f4675955353d0ff233b5fcbdce6cda
Aymeric RICHER <richayme1@gmail.com>
2025-12-02 14:28:22 -0500

    Création du tableau en 20 par 15

--- tp2/ex1/js/app.js
+++ tp2/ex1/js/app.js
@@ -8,7 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const h1 = document.createElement('h1')
   h1.textContent = 'Tableau de nombres'
   document.body.insertBefore(h1, table);
-  
+
+
+  for (let i = 0; i < 10; i++) {
+    const tr = document.createElement('tr');
+
+    for (let c = 0; c < 15; c++) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+
+
+
+
 
   
 
