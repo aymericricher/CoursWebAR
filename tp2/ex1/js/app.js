4c53feb22c006d01824e4665cb933fe71c9d66b6
Aymeric RICHER <richayme1@gmail.com>
2025-12-02 14:19:39 -0500

    Création du tableau vide et du titre de la page

--- tp2/ex1/js/app.js
+++ tp2/ex1/js/app.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+  
+  const table = document.createElement('table');
+  document.body.appendChild(table);
 
+  const h1 = document.createElement('h1')
+  h1.textContent = 'Tableau de nombres'
+  document.body.insertBefore(h1, table);
+  
 
+  
 
 });
