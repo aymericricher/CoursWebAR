dd2661fbcfe4aed08e0be9ac11e6c091804828d8
Aymeric RICHER <richayme1@gmail.com>
2025-12-03 17:39:23 -0500

    Installation de Bootstrap et le html du TP2 Complete

--- tp2/ex1/js/app.js
+++ tp2/ex1/js/app.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Création de variable
+  let compteur = 0;
 
-  
+  // Création du tableau
   const table = document.createElement('table');
   document.body.appendChild(table);
 
+  // Création du titre
   const h1 = document.createElement('h1')
   h1.textContent = 'Tableau de nombres'
   document.body.insertBefore(h1, table);
 
-  let compteur = 0;
 
+  
+
+  // Remplissage du tableau
   for (let i = 0; i < 10; i++) {
     const tr = document.createElement('tr');
 
@@ -18,9 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
       const td = document.createElement('td');
       tr.appendChild(td);
 
+      // Génération d'un nombre aléatoire entre 10 et 90
       const nombre = _.random(10, 90);
       td.textContent = nombre; 
 
+      // Mise en évidence des nombres supérieurs à 80
       if (nombre > 80) {
           td.classList.add('highlight')
           compteur++
@@ -30,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     table.appendChild(tr);
   }
 
+  // Affichage du compteur
   const p = document.createElement('p');
   p.id = 'compteur';
   p.textContent = `Nombre de cellules ayant un nombre supérieur à 80 : ${compteur}`;
